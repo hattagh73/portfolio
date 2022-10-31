ba63ad683ef74686d1fd46c05b7cf41aeaf08649
hattagh73 <>
2022-10-31 23:04:13 +0800

    update

--- components/project-details/PDetailsHero.tsx
+++ components/project-details/PDetailsHero.tsx
@@ -7,12 +7,12 @@ import { iPropProjectDetails } from '../../types';
 //* Import Project Details Page SCSS *//
 import s from '../../styles/pages/project-details/PDetailsHero.module.scss';
 
-
 const PDetailsHero = ({p_pro_details}:iPropProjectDetails) => {
 
     return (
         <section className={s.pj_details_hero}>
-            
+
+            {/* Hero Background Image */}
             <Image 
                 className={s.img_hero}
                 src={`https://res.cloudinary.com/dltslq5fh/image/upload/v1666973644/bg-hero/eyqldh9n0zcdv0jajkpc.svg`}
@@ -21,6 +21,7 @@ const PDetailsHero = ({p_pro_details}:iPropProjectDetails) => {
                 priority
             />
             
+            {/* Hero Project Titles */}
             <div className={`${s.contents_overlay} container`}>
                 <p>{p_pro_details.p_category}</p>
                 <h1>{p_pro_details.p_name}</h1>
