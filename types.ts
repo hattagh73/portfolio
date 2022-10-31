809cb125c6c7a4c36a9a183d90d2287edda889e6
hattagh73 <>
2022-10-31 22:15:01 +0800

    add file `types`

---
+++ types.ts
@@ -0,0 +1,15 @@
+export interface iPropProjects {
+    p_projects: iProjects[]
+}
+
+export interface iPropProjectDetails {
+    p_pro_details: iProjects
+}
+
+export interface iProjects {
+    p_id: number,
+    p_name: string,
+    p_category: string,
+    p_about?: string[]
+}
+
